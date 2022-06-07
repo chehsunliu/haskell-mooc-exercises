c2c8bf2d18020e66755153eb3bc2f625d2b669f3
chehsunliu <chehsunliu@gmail.com>
2022-06-07 15:11:59 +0800

    Complete Set6

--- Set6.hs
+++ Set6.hs
@@ -13,7 +13,10 @@ data Country = Finland | Switzerland | Norway
   deriving (Show)
 
 instance Eq Country where
-  (==) = todo
+  (==) Finland Finland = True
+  (==) Switzerland Switzerland = True
+  (==) Norway Norway = True
+  (==) _ _ = False
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement an Ord instance for Country so that
@@ -22,10 +25,13 @@ instance Eq Country where
 -- Remember minimal complete definitions!
 
 instance Ord Country where
-  compare = todo -- implement me?
-  (<=) = todo -- and me?
-  min = todo -- and me?
-  max = todo -- and me?
+  (<=) Finland Norway = True
+  (<=) Norway Finland = False
+  (<=) Norway Switzerland = True
+  (<=) Switzerland Norway = False
+  (<=) Finland Switzerland = True
+  (<=) Switzerland Finland = False
+  (<=) _ _ = True
 
 ------------------------------------------------------------------------------
 -- Ex 3: Implement an Eq instance for the type Name which contains a String.
@@ -41,7 +47,7 @@ data Name = Name String
   deriving (Show)
 
 instance Eq Name where
-  (==) = todo
+  (==) (Name name1) (Name name2) = map toLower name1 == map toLower name2
 
 ------------------------------------------------------------------------------
 -- Ex 4: here is a list type parameterized over the type it contains.
@@ -55,7 +61,9 @@ data List a = Empty | LNode a (List a)
   deriving (Show)
 
 instance Eq a => Eq (List a) where
-  (==) = todo
+  (==) Empty Empty = True
+  (==) (LNode v1 l1) (LNode v2 l2) = v1 == v2 && l1 == l2
+  (==) _ _ = False
 
 ------------------------------------------------------------------------------
 -- Ex 5: below you'll find two datatypes, Egg and Milk. Implement a
@@ -76,6 +84,16 @@ data Egg = ChickenEgg | ChocolateEgg
 data Milk = Milk Int -- amount in litres
   deriving (Show)
 
+class Price a where
+  price :: a -> Int
+
+instance Price Egg where
+  price ChickenEgg = 20
+  price ChocolateEgg = 30
+
+instance Price Milk where
+  price (Milk amount) = amount * 15
+
 ------------------------------------------------------------------------------
 -- Ex 6: define the necessary instance hierarchy in order to be able
 -- to compute these:
@@ -84,6 +102,13 @@ data Milk = Milk Int -- amount in litres
 -- price [Milk 1, Milk 2]  ==> 45
 -- price [Just ChocolateEgg, Nothing, Just ChickenEgg]  ==> 50
 -- price [Nothing, Nothing, Just (Milk 1), Just (Milk 2)]  ==> 45
+
+instance Price a => Price [a] where
+  price xs = sum $ map price xs
+
+instance Price a => Price (Maybe a) where
+  price (Just x) = price x
+  price Nothing = 0
 
 ------------------------------------------------------------------------------
 -- Ex 7: below you'll find the datatype Number, which is either an
@@ -94,6 +119,11 @@ data Milk = Milk Int -- amount in litres
 
 data Number = Finite Integer | Infinite
   deriving (Show, Eq)
+
+instance Ord Number where
+  (<=) _ Infinite = True
+  (<=) Infinite _ = False
+  (<=) (Finite a) (Finite b) = a <= b
 
 ------------------------------------------------------------------------------
 -- Ex 8: rational numbers have a numerator and a denominator that are
@@ -119,7 +149,7 @@ data RationalNumber = RationalNumber Integer Integer
   deriving (Show)
 
 instance Eq RationalNumber where
-  p == q = todo
+  (RationalNumber n1 d1) == (RationalNumber n2 d2) = n1 * d2 == n2 * d1
 
 ------------------------------------------------------------------------------
 -- Ex 9: implement the function simplify, which simplifies rational a
@@ -139,7 +169,14 @@ instance Eq RationalNumber where
 -- Hint: Remember the function gcd?
 
 simplify :: RationalNumber -> RationalNumber
-simplify p = todo
+simplify (RationalNumber a b) = RationalNumber (a `div` factor) (b `div` factor)
+  where
+    gcd a b
+      | a < 0 = - gcd (- a) b
+      | a > b = gcd b a
+      | a == 0 = b
+      | otherwise = gcd a (b - a)
+    factor = abs (gcd a b)
 
 ------------------------------------------------------------------------------
 -- Ex 10: implement the typeclass Num for RationalNumber. The results
@@ -160,12 +197,17 @@ simplify p = todo
 --   signum (RationalNumber 0 2)             ==> RationalNumber 0 1
 
 instance Num RationalNumber where
-  p + q = todo
-  p * q = todo
-  abs q = todo
-  signum q = todo
-  fromInteger x = todo
-  negate q = todo
+  (RationalNumber a1 b1) + (RationalNumber a2 b2) = simplify (RationalNumber (a1 * b2 + a2 * b1) (b1 * b2))
+  (RationalNumber a1 b1) * (RationalNumber a2 b2) = simplify (RationalNumber (a1 * a2) (b1 * b2))
+  abs (RationalNumber a b) = RationalNumber (abs a) (abs b)
+  signum (RationalNumber a b) = RationalNumber (signum' a) (signum' b)
+    where
+      signum' value
+        | value > 0 = 1
+        | value < 0 = -1
+        | otherwise = 0
+  fromInteger x = RationalNumber x 1
+  negate (RationalNumber a b) = RationalNumber (- a) b
 
 ------------------------------------------------------------------------------
 -- Ex 11: a class for adding things. Define a class Addable with a
@@ -179,6 +221,18 @@ instance Num RationalNumber where
 --   add 1 zero             ==>  1
 --   add [1,2] [3,4]        ==>  [1,2,3,4]
 --   add zero [True,False]  ==>  [True,False]
+
+class Addable a where
+  zero :: a
+  add :: a -> a -> a
+
+instance Addable Integer where
+  zero = 0
+  add x y = x + y
+
+instance Addable [a] where
+  zero = []
+  add x y = x ++ y
 
 ------------------------------------------------------------------------------
 -- Ex 12: cycling. Implement a type class Cycle that contains a
@@ -210,3 +264,20 @@ data Color = Red | Green | Blue
 
 data Suit = Club | Spade | Diamond | Heart
   deriving (Show, Eq)
+
+class Cycle a where
+  step :: a -> a
+  stepMany :: Int -> a -> a
+  stepMany 0 a = a
+  stepMany count a = stepMany (count - 1) (step a)
+
+instance Cycle Color where 
+  step Red = Green
+  step Green = Blue
+  step Blue = Red
+
+instance Cycle Suit where
+  step Club = Spade
+  step Spade = Diamond
+  step Diamond = Heart
+  step Heart = Club
